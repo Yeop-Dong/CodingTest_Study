9b1a153532fd728692b33058987eb351b06e316e
Dong-Yeop Lee <dong3855@gmail.com>
2025-09-15 21:54:53 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << (a + b) % c << endl;
+    cout << ((a % c) + (b % c)) % c << endl;
+    cout << (a * b) % c << endl;
+    cout << ((a % c) * (b % c)) % c << endl;
+    return 0;
+}
