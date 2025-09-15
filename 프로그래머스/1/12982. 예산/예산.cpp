66a8a450cb91fb57d9d48baa958aa278e34468e5
Dong-Yeop Lee <dong3855@gmail.com>
2025-09-15 22:13:13 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 3.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    for(auto b : d){
+        budget -= b;
+        if (budget < 0) break;
+        answer++;
+    }
+    return answer;
+}
