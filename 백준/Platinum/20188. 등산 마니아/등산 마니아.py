b0eba71b66b41d3bd937651f07bad3ad7c6f2b25
Dong-Yeop Lee <dong3855@gmail.com>
2025-10-05 21:19:16 +0900

    [Platinum V] Title: 등산 마니아, Time: 1228 ms, Memory: 144472 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/20188. 등산 마니아/등산 마니아.py
+++ 백준/Platinum/20188. 등산 마니아/등산 마니아.py
@@ -3,29 +3,20 @@ sys.setrecursionlimit(10**9)
 input = lambda: sys.stdin.readline().rstrip()
 n = int(input())
 con = [[] for _ in range(n+1)]
+global ans
+ans = 0
 for i in range(n - 1):
     a, b = map(int, input().split())
-    con[a].append((i, b))
-    con[b].append((i, a))
-root = (1, -1, [])
-def contree(node: tuple[int, int, list] = root):
-    v, p, ch = node
-    for i, nv in con[v]:
-        if nv == p: continue
-        newnode = (nv, v, [])
-        contree(newnode)
-        ch.append((i, newnode))
-    node = v, p, ch
-paths = [[] for _ in range(n+1)]
-def findpaths(node: tuple[int, int, list] = root, path: list = []):
-    paths[node[0]] = path
-    for e, nnode in node[2]:
-        findpaths(nnode, path + [e])
-contree()
-findpaths()
-paths = [set(p) for p in paths]
-result = 0
-for i in range(1, n+1):
-    for j in range(i+1, n+1):
-        result += len(paths[i] | paths[j])
-print(result)
+    con[a].append(b)
+    con[b].append(a)
+def dfs(v: int, p: int):
+    global ans
+    cnt = 1
+    for nv in con[v]:
+        if p == nv: continue
+        sub = dfs(nv, v)
+        ans += sub * (n - sub) + sub * (sub - 1) // 2
+        cnt += sub
+    return cnt
+dfs(1, 1)
+print(ans)
